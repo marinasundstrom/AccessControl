007e6c9aa13e4768fb622fac961d4b89426c937a
Robert Sundström <robert.sundstrom@outlook.com>
2021-02-14 14:15:00 +0100

    Update config and publish scripts.

--- src/AccessPoint/AccessPoint/Program.cs
+++ src/AccessPoint/AccessPoint/Program.cs
@@ -3,18 +3,29 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using AccessPoint.Application.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace AccessPoint
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var app = CreateWebHostBuilder(args).Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<AccessPointContext>();
+
+                await context.Database.EnsureCreatedAsync();
+            }
+
+            await app.RunAsync();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
--- src/AccessPoint/AccessPoint/publish.sh
+++ src/AccessPoint/AccessPoint/publish.sh
@@ -1,4 +1,4 @@
 dotnet publish -r linux-arm /p:ShowLinkerSizeComparison=true 
-pushd ./bin/Debug/netcoreapp2.2/linux-arm/publish
+pushd ./bin/Debug/net5.0/linux-arm/publish
 scp -p raspberry * pi@raspberrypi.local:/home/pi/AccessPoint
 popd

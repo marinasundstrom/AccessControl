e4a23d5a9d42320930fbe485399d91d9553eb761
Marina Sundström <marna.li@outlook.com>
2022-06-22 15:37:28 +0200

    Fix login

--- src/AppService/AppService.Domain/Entities/User.cs
+++ src/AppService/AppService.Domain/Entities/User.cs
@@ -7,6 +7,6 @@ namespace AppService.Domain.Entities
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        public string RefreshToken { get; set; }
+        public string? RefreshToken { get; set; }
     }
 }
